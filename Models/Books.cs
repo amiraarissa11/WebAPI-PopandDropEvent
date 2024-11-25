84998cfe645d93a4c4f45c9509c18c445a8572c6
amiraarissa11 <92544838+amiraarissa11@users.noreply.github.com>
2024-11-26 06:30:33 +0800

    Add files via upload

---
+++ Models/Books.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookDonorsAPI.Models
+{
+    public class Book
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Tittle { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Author { get; set; }
+
+        [MaxLength(100)]
+        public string Genre { get; set; }
+
+        [Range(1450, 2100, ErrorMessage = "Please enter a valid year.")]
+        public int YearPublished { get; set; }
+    }
+}
---
+++ Models/Donors.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookDonorsAPI.Models
+{
+    public class Donor
+    {
+        [Key]
+        public int DonorID { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [EmailAddress]
+        [MaxLength(100)]
+        public string Email { get; set; }
+
+        [Phone]
+        [MaxLength(20)]
+        public string Phone { get; set; }
+
+        [MaxLength(255)]
+        public string BookTittle { get; set; }
+    }
+}
